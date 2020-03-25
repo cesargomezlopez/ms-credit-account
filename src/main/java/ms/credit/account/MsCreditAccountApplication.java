8d7f573fe26f6f62d9e8bf90519b602ef9acf98a
Cesar Gomez <cesar.gomez@urp.edu.pe>
2020-03-25 15:19:25 -0500

    Project created

---
+++ src/main/java/ms/credit/account/MsCreditAccountApplication.java
@@ -0,0 +1,13 @@
+package ms.credit.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCreditAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCreditAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ms/credit/account/MsCreditAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.credit.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCreditAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
