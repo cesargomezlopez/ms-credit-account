6e160ce65ae1a3cdcdca3b0f32138da0d84b8d1f
Cesar Gomez <cesar.gomez@urp.edu.pe>
2020-04-05 00:12:52 -0500

    Eureka Client Configuration

--- src/main/java/ms/credit/account/MsCreditAccountApplication.java
+++ src/main/java/ms/credit/account/MsCreditAccountApplication.java
@@ -2,12 +2,14 @@ package ms.credit.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MsCreditAccountApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsCreditAccountApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(MsCreditAccountApplication.class, args);
+  }
 
 }
--- src/test/java/ms/credit/account/MsCreditAccountApplicationTests.java
+++ src/test/java/ms/credit/account/MsCreditAccountApplicationTests.java
@@ -2,12 +2,14 @@ package ms.credit.account;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootTest
 class MsCreditAccountApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
